c0ffc3630906004f89f206d3492c99fb87b8e25e
Osura Rathnayake <osuranew@gmail.com>
2023-07-22 16:31:50 +0800

    Update example1.tf

--- example1.tf
+++ example1.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-
-region     = "ap-northeast-2"
-  access_key = ""
-  secret_key = ""
-}
-
 resource "aws_instance" "vm-terraform" {
   ami           = "ami-0a306845f8cfbd41a"
   instance_type = "t2.micro"
